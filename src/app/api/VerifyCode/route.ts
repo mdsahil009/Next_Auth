449c294762e7529e46f5b123299893d21a12d365
mdsahil009 <mohdsahil00736@gmail.com>
2025-05-11 10:56:25 +0530

    add Verification code route

---
+++ src/app/api/VerifyCode/route.ts
@@ -0,0 +1,68 @@
+import dbConnect from "@/app/lib/dbConnect";
+import UserModel from "@/app/Models/user";
+import { any } from "zod";
+
+export async function POST(request: Request){
+    await dbConnect()
+
+    try {
+        const {username, code} = await request.json()
+
+        const decodedUsername = decodeURIComponent{username}
+
+        const user = await UserModel.findOne({username: decodedUsername})
+
+
+        if (!user){
+            return Response.json(
+            {
+            success: false,
+            message: "User not found"
+            },
+            {status: 500}
+        )
+        }
+
+        const isCodeValid = user.verifyCode === code
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date()
+
+        if(isCodeValid && isCodeNotExpired) {
+            user.isVerified = true
+            await user.save()
+            
+            return Response.json(
+            {
+            success: true,
+            message: "Account verified Successfully"
+            },
+            {status: 200}
+            )
+        }else if (!isCodeNotExpired) {
+            return Response.json(
+            {
+            success: false,
+            message: "Verification code has expired"
+            },
+            {status: 400}
+            )
+        }else {
+            return Response.json(
+            {
+            success: false,
+            message: "Incorrect verification code"
+            },
+            {status: 500}
+        )
+        }
+
+    } catch (error) {
+        console.error("Error verifying user", error)
+        return Response.json(
+            {
+            success: false,
+            message: "Error verifying user"
+            },
+            {status: 500}
+        )
+    }
+}
